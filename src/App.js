acb9e9bd5c43a513dcba08a89eae7a463e9d8d50
Adam COURTARO <101117765+adamctr@users.noreply.github.com>
2023-01-16 20:22:15 +0100

    final working version

--- src/App.js
+++ src/App.js
@@ -1,7 +1,86 @@
 import "./App.css";
-import { useState, useEffect, useRef } from "react";
+import { useState, useEffect } from "react";
 import TitleKey from "./Components/TitleKey";
 import Key from "./Components/Key";
+
+const SEPARATOR = "%%%";
+const KEY_SEP = "[Touch]";
+
+const splitKeySentence = (sentence) => sentence.split(SEPARATOR);
+
+const showKeyTemp = (key) => `${SEPARATOR}${KEY_SEP}${key}${SEPARATOR}`;
+
+const quotes = [
+  "Le succès n'est pas final, l'échec n'est pas fatal : c'est le courage qui compte.",
+  "L'échec est un tournant vers la réussite.",
+  "Il n'y a pas de succès sans échec.",
+  "L'échec est une opportunité de recommencer avec plus d'expérience.",
+  "Chaque échec est un atout pour la prochaine réussite.",
+];
+
+const solutionsForLetter = {
+  "@": `Appuyer en même temps sur les touches ${showKeyTemp(
+    "Alt Gr"
+  )} et ${showKeyTemp(`0 à,@`)}`,
+  " ": `Appuyer sur la barre espace ${showKeyTemp(
+    " "
+  )}, qui est la plus  longue barre, situé tout au milieu, 
+   tout en bas du clavier.`, // Barre Espace
+  backspace: `Appuyer sur la touche Effacer ${showKeyTemp(
+    "backspace"
+  )}. Elle se trouve au dessus de la touche Entrer ${showKeyTemp("enter")}`,
+  enter: `Appuyer sur la touche Entrer ${showKeyTemp(
+    "enter"
+  )} pour valider la saisie`,
+  1: `Appuyer en même temps sur la touche Majuscule ${showKeyTemp(
+    "shift"
+  )} et ${showKeyTemp(`1 &`)}`,
+  2: `Appuyer en même temps sur la touche Majuscule ${showKeyTemp(
+    "shift"
+  )} et ${showKeyTemp(`2 é,~`)}`,
+  3: `Appuyer en même temps sur la touche Majuscule ${showKeyTemp(
+    "shift"
+  )} et ${showKeyTemp(`3 ",#`)}`,
+  4: `Appuyer en même temps sur la touche Majuscule ${showKeyTemp(
+    "shift"
+  )} et ${showKeyTemp(`4 ',{`)}`,
+  5: `Appuyer en même temps sur la touche Majuscule ${showKeyTemp(
+    "shift"
+  )} et ${showKeyTemp("5 (,[")}`,
+  6: `Appuyer en même temps sur la touche Majuscule ${showKeyTemp(
+    "shift"
+  )} et ${showKeyTemp(`6 _,|`)}`,
+  7: `Appuyer en même temps sur la touche Majuscule ${showKeyTemp(
+    "shift"
+  )} et ${showKeyTemp("7 è,`")}`,
+  8: `Appuyer en même temps sur la touche Majuscule ${showKeyTemp(
+    "shift"
+  )} et ${showKeyTemp(`8 _,\ `)}`,
+  9: `Appuyer en même temps sur la touche Majuscule ${showKeyTemp(
+    "shift"
+  )} et ${showKeyTemp(`9 ç,^`)}`,
+  0: `Appuyer en même temps sur la touche Majuscule ${showKeyTemp(
+    "shift"
+  )} et ${showKeyTemp(`0 à,@`)}`,
+  é: `Appuyer sur la touche ${showKeyTemp("2 é,~")}`,
+  è: `Appuyer sur la touche ${showKeyTemp("7 è,`")}`,
+  à: `Appuyer sur la touche ${showKeyTemp("0 à,@")}`,
+  "-": `Appuyer sur la touche ${showKeyTemp("6 -,|")}`,
+  _: `Appuyer sur la touche ${showKeyTemp(`8 _,\ `)}`,
+  "!": `Appuyer sur la touche ${showKeyTemp(
+    `§ !`
+  )} qui se trouve en haut à droite de la barre espace ${showKeyTemp(" ")}`,
+  "?": `Appuyer sur la touche Majuscule ${showKeyTemp(
+    "shift"
+  )} et ${showKeyTemp(
+    "? ,"
+  )} qui se trouve en haut à droite de la barre espace`,
+  ".": `Appuyer sur la touche Majuscule ${showKeyTemp(
+    "shift"
+  )} et ${showKeyTemp(
+    ". ;"
+  )} qui se trouve en haut à droite de la barre espace`,
+};
 
 function App() {
   const [inputValue, setInputValue] = useState("");
@@ -10,14 +89,19 @@ function App() {
   const [popupContent, setPopupContent] = useState(null);
   const [timeoutId, setTimeoutId] = useState(null);
 
-  const solutionsForLetter = {
-    "@": "Appuyer en même temps sur la touche Alt Gr et 0.", // @
-    " ": "Appuyer sur la barre espace, qui est la plus {<Key>Alt Gr</Key>} grande barre, toute en longueur, situé tout au milieu, tout en bas du clavier.", // Barre Espace
-  };
-
   useEffect(() => {
     getRandomQuote();
   }, []);
+
+  useEffect(() => {
+    setPopupContent(
+      `Pour écrire ${
+        quote[0]
+      } en majuscule, appuyer en même temps sur la touche Majuscule ${showKeyTemp(
+        "shift"
+      )} et ${showKeyTemp(afterLetter())}`
+    );
+  }, [quote]);
 
   useEffect(() => {
     numberOfErrors();
@@ -27,7 +111,8 @@ function App() {
 
     setTimeoutId(
       setTimeout(() => {
-        if (inputValue !== "") {
+        //Si il y a du texte dans l'input et que la solution pour la lettre d'après existe
+        if (inputValue !== "" && solutionsForLetter[afterLetter()]) {
           setPopupContent(solutionsForLetter[afterLetter()]);
         }
       }, 3000)
@@ -43,7 +128,7 @@ function App() {
   const handleChange = (event) => {
     setInputValue(event.target.value);
 
-    // Si la lettre tapée est la valeur attendue et que popupContent !== null, faire setPopupContent(null)
+    // Réinitialise le popup
     let quoteArr = quote.split("");
     let inputArr = inputValue.split("");
     if (
@@ -52,9 +137,10 @@ function App() {
     ) {
       setPopupContent(null);
     }
+
+    //Si l'input est complet, afficher l'astuce Entrer
     if (quote.length - 1 === inputValue.length) {
-      getRandomQuote();
-      setInputValue("");
+      setPopupContent(solutionsForLetter["enter"]);
     }
   };
 
@@ -65,6 +151,7 @@ function App() {
     inputArr.forEach((letter, index) => {
       if (letter !== quoteArr[index]) {
         error++;
+        setPopupContent(solutionsForLetter["backspace"]);
       }
     });
     setErrorState(error);
@@ -72,21 +159,20 @@ function App() {
 
   const handleKeyDown = (event) => {
     // Vérifie si la touche pressée est un chiffre
-    if (errorState >= 1 && event.keyCode !== 8) {
+    if (errorState && event.keyCode !== 8) {
       // Empêche la saisie de la touche
       event.preventDefault();
     } else if (event.key === "Backspace") {
       return true;
     }
-  };
 
-  const quotes = [
-    "Le succès n'est pas final, l'échec n'est pas fatal : c'est le courage qui compte.",
-    "L'échec est un tournant vers la réussite.",
-    "Il n'y a pas de succès sans échec.",
-    "L'échec est une opportunité de recommencer avec plus d'expérience.",
-    "Chaque échec est un atout pour la prochaine réussite.",
-  ];
+    //Si l'input est complet, faire Entrer pour valider
+    if (quote.length === inputValue.length && event.keyCode === 13) {
+      getRandomQuote();
+      setInputValue("");
+      setPopupContent(null);
+    }
+  };
 
   const getRandomQuote = () => {
     return setQuote(quotes[Math.floor(Math.random() * quotes.length)]);
@@ -109,8 +195,8 @@ function App() {
 
   return (
     <>
-      <div className="App h-[100vh] ">
-        <header className=" flex justify-center items-center h-32 max-sm:flex-col max-sm:w-[100vw] max-sm:mt-0 mt-5 rounded w-[90vw] mx-auto bg-green p-5 text-white ">
+      <div className="App ">
+        <header className=" flex justify-center items-center h-32 max-sm:flex-col max-sm:w-[100vw] max-sm:mt-0  rounded  mx-auto bg-green p-5 text-white ">
           <h1 className="text-[2.2rem] max-sm:text-[2.3rem] text-center rounded ">
             AIDE MOI À &nbsp;
           </h1>
@@ -122,7 +208,7 @@ function App() {
             <TitleKey>R</TitleKey>
           </div>
         </header>
-        <main className="flex mt-16 items-center h-[60vh] flex-col ">
+        <main className="flex mt-16 items-center h-[100vh] flex-col ">
           <div>
             <p className="text-4xl max-sm:text-[1.8rem] p-5 font-semibold  text-center bg-white rounded">
               {renderQuote()}
@@ -135,22 +221,35 @@ function App() {
             value={inputValue}
             onChange={handleChange}
             onKeyDown={handleKeyDown}
+            maxLength={quote.length}
           />
           {popupContent && (
-            <div className=" mx-auto rounded mt-14 w-[80vw] text-2xl p-5 bg-darkpurple text-white font-semibold">
-              <div className="font-bold px-4 py-1 rounded w-fit mb-4 bg-green">
-                ASTUCE :
+            <div className=" mx-auto astuce rounded mt-14 w-[80vw] text-2xl max-md:text-xl p-5 bg-darkpurple text-white font-semibold">
+              <div className=" font-bold px-4 py-1 rounded w-fit mb-4 bg-green">
+                ASTUCE :{" "}
+                {errorState ? (
+                  "Effacer"
+                ) : (
+                  <span className="text-red-500">{afterLetter()}</span>
+                )}
               </div>
-              {popupContent}
+              {splitKeySentence(popupContent).map((sentencePart) =>
+                sentencePart.startsWith(KEY_SEP) ? (
+                  <Key key={sentencePart}>
+                    {sentencePart.replace(KEY_SEP, "")}
+                  </Key>
+                ) : (
+                  sentencePart
+                )
+              )}
             </div>
           )}
         </main>
+        <footer className="bg-green text-darkpurple flex p-5 justify-center items-center">
+          Copyright ©2022 Aide moi à taper. Tous droits réservés. Créé par Adam
+          Courtaro |
+        </footer>
       </div>
-
-      <footer className="bg-green text-darkpurple flex p-5 justify-center items-center">
-        Copyright ©2022 Aide moi à taper. Tous droits réservés. Créé par Adam
-        Courtaro |
-      </footer>
     </>
   );
 }
--- src/Components/Key.js
+++ src/Components/Key.js
@@ -1,19 +1,70 @@
 import React from "react";
 import key from "../key.png";
+import spacebar from "../spacebar.png";
+import backspace from "../backspace.png";
+import enter from "../enter.png";
+import shift from "../shift.png";
 
-function Key({ children }) {
-  return (
-    <div className="relative border-5 w-14 h-14 mx-1 ">
-      <img
-        className=" absolute left-[50%] top-[50%] translate-x-[-50%] translate-y-[-50%]"
-        src={key}
-        alt=""
-      />
-      <div className="absolute text-[1.3rem] leading-5  pb-[5%] font-bold text-black left-[50%] top-[50%] translate-x-[-50%] translate-y-[-50%]">
-        {children}
-      </div>
-    </div>
-  );
-}
-
-export default Key;
+export default ({ children }) => {
+  switch (children) {
+    case " ":
+      return (
+        <div className="relative inline-block border-5  align-middle w-28 mx-1">
+          <img
+            className=" absolute left-[50%] top-[50%]  translate-x-[-50%] translate-y-[-50%]"
+            src={spacebar}
+            alt={"Touche de clavier intitulé Barre Espace"}
+          />
+          <div className="absolute text-[1.3rem] leading-5  pb-[5%] font-bold text-black left-[50%] top-[50%] translate-x-[-50%] translate-y-[-50%]">
+            {children}
+          </div>
+        </div>
+      );
+    case "backspace":
+      return (
+        <div className="relative inline-block border-5 align-middle  w-14 h-14 mx-1">
+          <img
+            className=" absolute left-[50%] top-[50%] translate-x-[-50%] translate-y-[-50%]"
+            src={backspace}
+            alt={"Touche de clavier intitulé Effacer"}
+          />
+          <div className="absolute text-[1.3rem] leading-5  pb-[5%] font-bold text-black left-[50%] top-[50%] translate-x-[-50%] translate-y-[-50%]"></div>
+        </div>
+      );
+    case "shift":
+      return (
+        <div className="relative inline-block border-5 align-middle  w-20 h-14 mx-1">
+          <img
+            className=" absolute left-[50%] top-[50%] translate-x-[-50%] translate-y-[-50%]"
+            src={shift}
+            alt={"Touche de clavier intitulé Effacer"}
+          />
+          <div className="absolute text-[1.3rem] leading-5  pb-[5%] font-bold text-black left-[50%] top-[50%] translate-x-[-50%] translate-y-[-50%]"></div>
+        </div>
+      );
+    case "enter":
+      return (
+        <div className="relative inline-block border-5 align-middle  w-14 h-14 mx-1">
+          <img
+            className=" absolute left-[50%] top-[50%] translate-x-[-50%] translate-y-[-50%]"
+            src={enter}
+            alt={"Touche de clavier intitulé Effacer"}
+          />
+          <div className="absolute text-[1.3rem] leading-5  pb-[5%] font-bold text-black left-[50%] top-[50%] translate-x-[-50%] translate-y-[-50%]"></div>
+        </div>
+      );
+    default:
+      return (
+        <div className="relative inline-block border-5 align-middle  w-14 h-14 mx-1">
+          <img
+            className=" absolute left-[50%] top-[50%] translate-x-[-50%] translate-y-[-50%]"
+            src={key}
+            alt={"Touche de clavier intitulé " + { children }}
+          />
+          <div className="absolute text-[1.3rem] leading-5  pb-[5%] font-bold text-black left-[50%] top-[50%] translate-x-[-50%] translate-y-[-50%]">
+            {children}
+          </div>
+        </div>
+      );
+  }
+};
